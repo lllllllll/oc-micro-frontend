e2adbdb2544441c602d5793bebb52dba31a0d5b9
YAKA <hnudka@gmail.com>
2021-06-11 18:03:55 +0700

    First commit

---
+++ components/content/view.js
@@ -0,0 +1,7 @@
+import Css from './css/styles.css';
+
+export default () =>
+  `<div class="${Css.content}">
+    <h1>Content</h1>
+    <p>D:\TESTs\OpenComponents\components\content\css\styles.css</p>
+  </div>`;
---
+++ components/header/view.js
@@ -0,0 +1,8 @@
+import styles from './css/styles.css';
+// import './css/styles.css';
+
+export default ({ name, staticPath }) =>
+  `<div class=${styles.awesome}>
+    Hello ${name}!
+    <img src="${staticPath}img/logo.png" />
+  </div>`;
